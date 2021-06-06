68bdbe1254ef71e0bd8ce431c9e386953a17c78f
DinhNPVy <79675139+DinhNPVy@users.noreply.github.com>
2021-06-06 08:42:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ for(i = 0; i <= 100; i++){
     }else{
         if(i % 3 == 0){
             document.write(i + " Fizz <br>");
-        }
-        else if(i % 5 == 0){
+        }else if(i % 5 == 0){
             document.write(i + " Buzz <br>")
         }
     }
